a49059478f1a18204301ff0d15945894e0d3af27
eahishak <146794224+eahishak@users.noreply.github.com>
2024-05-21 18:26:15 -0400

    Add files via upload

---
+++ education.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <meta name="HandheldFriendly" content="true">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+        <META http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Jackal Services</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Slider
+    ================================================== -->
+<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css" href="css/Cssindex.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+<link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+  <div class="body-content">
+  <div class="headerside">
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand page-scroll" href="#page-top">Jackal Services</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about" class="page-scroll">About US</a></li>
+        <li><a href="#services" class="page-scroll">Services</a></li>
+        <li><a href="#portfolio" class="page-scroll">Job Opportunities</a></li>
+        <li><a href="#testimonials" class="page-scroll">Bookings</a></li>
+        <li><a href="#contact" class="page-scroll">Online Shoppings</a></li>
+        <li><a href="#login" class="page-scroll">Login</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+</nav>
+</div>
+
+<!-- Header -->
+<div class="Education">
+  <div class="sidebar-1">
+    <h2 id="student">STUDENTS</h2>
+                
+                <ol class="student-page-list">
+                    
+                          
+                           <li id="student-page-list"><div class="dropdown">
+                            <button class="dropbtn" id="apply">APPLY NOW</button>
+          
+                        
+                           <div class="dropdown-content">
+                              <a href="chooseCombination.html" target="_blank">
+                                <p id="choose_comb">Choose Combination </p><br></a>
+                              <a href="changeCombination.html" target="_blank">
+                                <p id="change_comb">Change Combination</p><br></a>
+                              <a href="commitmentForm.html" target="_blank">
+                              <p id="commitment_frm">Commitment Form</p></a>
+                          
+                        </div>
+                     </div>
+                   </li>
+                    <li id="student-page-list"><a href="https://academicbridge.xyz/" target="_blank"> Academic Bridge</a></li>
+
+                    <li id="student-page-list">target="_blank">Office 365 Portfolio</a></li>
+
+                    <li id="student-page-list"><a href="https://elearning.reb.rw/" target="_blank">Reb Links</a></li>
+                    <li id="student-page-list"><a href="https://www.youtube.com/watch?v=P6g8rFOKMcU" target="_blank">Video Lessons</a></li> 
+                 
+                    <li id="student-page-list"><a href="https://lichess.org/ishE1EJ01Jwz" target="_blank">Games (Scrable and chess)</a></li>
+
+                    <li id="student-page-list"><a href="https://www.gettyimages.com/photos/agahozo-shalom-youth-village?phrase=agahozo%20shalom%20youth%20village&sort=mostpopular" target="_blank">Photo Gallery</a></li>
+
+                     <li id="student-page-list"><a href="https://kahoot.com/" target="_blank">Kahoot Games</a></li>
+                </ol>
+                
+    
+  </div>
+  <div class="sidebar-2">
+    
+  </div>
+</div>
+  </div>
+
+<!-- Footer Section -->
+<div class="footer">
+  <div class="fs-address">
+    <h3 id="list6" style="color:#ffffff;"><marquee scrollamount="20" direction="right">Jackal SP Address</marquee></h3><hr>
+                     <span id="list7">Independant Ave, NY 20585-0270, United States<br></span> 
+                    <span id="list8">KG 145St, Kacyiru <br></span>
+                        <span id="list9">Kigali<br></span>
+                        <span id="list10">Rwanda<br></span> </b>
+  </div>
+  <div class="fs-generalinfor">
+    <div id="follow"><p style="font-size: 1.3em!important">Follow Us:
+            <a href="#" target="_blank" class="fa fa-facebook"></a>
+            <a href="#" target="_blank" class="fa fa-twitter"></a>
+            <a href="#" target="_blank" class="fa fa-linkedin"></a>
+            <a href="#" target="_blank" class="fa fa-youtube"></a>
+            <a href="#" target="_blank" class="fa fa-instagram"></a>
+            <h3 class="course-family">
+                <a href="#" target="_blank"><span id="family" style="color: white !important">Home<br></span></a>
+                <a href="#" target="_blank">
+                  <span id="family" style="clear: both!important; color: white !important"><span id="family">Services<br></span></a>
+                <a href="#" target="_blank" style="color: white !important"> <span id="family">Networks<br></span></a> 
+                <a href="#" target="_blank" style="color: white !important"><span id="family">Shoppings</span></a>
+            </h3>
+            </div>
+    
+  </div>
+  <div class="sign-up">
+    <form action="new.html" method="post|get">
+                   <label class="comment"><p style="font-size: 1.5em!important">Sign Up for our mailing list</p> </label>
+                   <input type="text" name="YourNames" size="25" placeholder="Full Name" required><br>
+                   <input type="text" name="email" size="25" placeholder="Email Address" required><br>
+                        <textarea  id="comment" name="comment" rows="4" cols="50" required="please leave a comment" placeholder="Choose fields of your desired information">  
+                    
+                    </textarea>
+                    <input type="submit" value="Send">
+                </form>
+  </div>
+  <div class="copyright">
+    <p>Copyright &copy2023&copy Jackal Service Provider<a href="http://www.templatewire.com" rel="nofollow"></a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+</div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+<script type="text/javascript" src="js/bootstrap.js"></script> 
+<script type="text/javascript" src="js/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+<script type="text/javascript" src="js/jquery.isotope.js"></script> -
+<script type="text/javascript" src="js/owl.carousel.js"></script> 
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js/contact_me.js"></script> 
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
